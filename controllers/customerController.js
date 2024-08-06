59befcd5df02e1b452fea24bdd9de1ad3b9a6f5d
abhijeetdrapcode <abhijeet@drapcode.com>
2024-08-06 17:23:58 +0530

    Made a seprate route to test if the payment is processing or not

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -9,35 +9,65 @@ exports.createCustomer = async (req, res) => {
     });
 
     const customerId = customer.id;
-    const customers = await stripe.customers.list({
-        limit: 3,
-        });
-        console.log(customers)
 
     const setupIntent = await stripe.setupIntents.create({
       customer: customerId,
-      payment_method_types: ['us_bank_account'],
+      payment_method_types: ["us_bank_account"],
       payment_method_options: {
         us_bank_account: {
           financial_connections: {
-            permissions: ['payment_method', 'balances'],
+            permissions: ["payment_method", "balances"],
           },
         },
       },
     });
 
-    res.json({ clientSecret: setupIntent.client_secret, customer: customer.id });
+    res.json({
+      clientSecret: setupIntent.client_secret,
+      customer: customer.id,
+    });
   } catch (error) {
     console.error("Error creating customer: ", error);
     res.status(500).send("Internal Server Error");
   }
 };
 
+exports.createPayment =  async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1099,
+      currency: 'usd',
+      payment_method_types: ["us_bank_account"],
+      payment_method_options: {
+        us_bank_account: {
+          financial_connections: {
+            permissions: ["payment_method", "balances"],
+          },
+        },
+      },
+    });
+
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
 
 exports.renderTestPage = (req, res) => {
-  res.render('index',{publishableKey});
+  res.render("index", { publishableKey });
 };
 
-exports.renderPaymentPage = (req, res) =>{
-  res.render('completePayment',{publishableKey});
+exports.renderPaymentPage = (req, res) => {
+  res.render("completePayment", { publishableKey });
+};
+
+
+exports.renderTestingPage = (req,res)=>{
+  res.render("testing",{publishableKey})
 }
+
+
+exports.renderSuccessPage = (req,res)=>{
+  res.render("success",{publishableKey})
+}
+
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -5,7 +5,15 @@ const customerController = require('../controllers/customerController');
 
 
 router.get('/', customerController.createCustomer);
+
 router.get('/testing', customerController.renderTestPage,);
+
 router.get('/payment',customerController.renderPaymentPage);
+
+router.get('/pay',customerController.renderTestingPage);
+
+router.get('/success',customerController.renderSuccessPage);
+
+router.post('/create-payment-intent', customerController.createPayment);
 
 module.exports = router;
